863f129a7bd8e8d7522ae0b9e33d4ee02c71ee19
Ivan_Kalinyuk <ivan_kalinyuk01@mail.ru>
2022-08-07 02:53:16 +0300

    Написали программу, показывающую, какое число больше, а какое меньше

---
+++ №2/Program.cs
@@ -0,0 +1,26 @@
+﻿int max = 0;
+int min = 0;
+Console.Write("Введите значение a = ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение b = ");
+int b = Convert.ToInt32(Console.ReadLine());
+if(a>b)
+{
+    max = a;
+    min = b;
+    Console.Write("max = ");
+    Console.WriteLine(a);
+    Console.Write("min = ");
+    Console.WriteLine(b);
+}
+else
+{
+    max = b;
+    min = a;
+    Console.Write("max = ");
+    Console.WriteLine(b);
+    Console.Write("min = ");
+    Console.WriteLine(a);
+}
+
+
