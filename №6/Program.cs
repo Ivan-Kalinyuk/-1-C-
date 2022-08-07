bdeb8afc5a5aed3b1d22ede396f52da13cf46c5e
Ivan_Kalinyuk <ivan_kalinyuk01@mail.ru>
2022-08-07 04:15:46 +0300

    Написали программу, проверяющую введеное пользователем число на четность/нечетность

---
+++ №6/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите значение a = ");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a%2 == 0)
+{
+    Console.Write("Ваше число - четное");
+
+}
+else 
+{
+    Console.Write("Ваше число - нечетное");
+}
