ca4e9f56a9bb323c7b5b41d88490ace55b7d0d79
Ivan_Kalinyuk <ivan_kalinyuk01@mail.ru>
2022-08-07 03:45:22 +0300

    Написали программу, выводящую на экран максимальное число из 3 чисел, введенных пользователем ранее

---
+++ №4/Program.cs
@@ -0,0 +1,39 @@
+﻿Console.Write("Введите значение a = ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение b = ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение c = ");
+int c = Convert.ToInt32(Console.ReadLine());
+int max = a;
+
+    if(b>a) 
+    {
+        max = b;
+        if (c>b)
+        {
+            max = c;
+        }
+        Console.Write("max = ");
+        Console.WriteLine(max);
+        
+    }
+    else
+    {
+        if (c>a)
+        {
+            max = c;
+        }
+        else 
+        {
+            max = a;
+        }
+        Console.Write("max = ");
+        Console.WriteLine(max);
+    }
+
+
+
+
+
+
+
