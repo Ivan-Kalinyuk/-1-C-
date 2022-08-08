83e0435542fcba226fa49519568c6b180722871f
Ivan_Kalinyuk <ivan_kalinyuk01@mail.ru>
2022-08-08 20:07:14 +0300

    Написали программу, выводяющую все четные числа от 1 до того числа, которое ввел пользователь

---
+++ №8/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Введите любое целое число, больше единицы - ");
+int N = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+while (i<N)
+{
+    if (i % 2 == 0) 
+    {
+        Console.Write(i);
+        Console.Write(", ");
+        i = i + 1;
+
+    }
+    else 
+    {
+        i = i + 1;
+    }
+}
+
